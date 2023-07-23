7b0903eff5c225a595f8cd3968dce5262e1e5b47
NIRUPAMA G <99091630+Niru511@users.noreply.github.com>
2023-07-23 12:06:36 +0530

    Update utils.py

--- candidate_management/candidates/utils.py
+++ candidate_management/candidates/utils.py
@@ -3,19 +3,26 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 import base64
 
-def encrypt_data(data, public_key_path):
-    with open(public_key_path, 'rb') as f:
+def encrypt_string_rsa(message):
+    byte_message = str.encode(message)
+    with open('public_key.pem', 'rb') as f:
         public_key = RSA.import_key(f.read())
-        cipher_rsa = PKCS1_OAEP.new(public_key)
-        encrypted_data = cipher_rsa.encrypt(data.encode('utf-8'))
-        return base64.b64encode(encrypted_data).decode('utf-8')
 
-def decrypt_data(encrypted_data, private_key_path):
-    encrypted_data = base64.b64decode(encrypted_data)
-    with open(private_key_path, 'rb') as f:
+    cipher = PKCS1_OAEP.new(public_key)
+    encrypted_string = cipher.encrypt(byte_message)
+    encrypted_string = base64.b64encode(encrypted_string).decode('utf-8')
+    return encrypted_string
+
+
+def decrypt_string_rsa(encrypted_string):
+    decoded_hash_string = base64.b64decode(encrypted_string)
+    with open('private_key.pem', 'rb') as f:
         private_key = RSA.import_key(f.read())
-        cipher_rsa = PKCS1_OAEP.new(private_key)
-        decrypted_data = cipher_rsa.decrypt(encrypted_data)
-        return decrypted_data.decode('utf-8')
+
+    cipher = PKCS1_OAEP.new(private_key)
+    result_string = cipher.decrypt(decoded_hash_string).decode()
+
+    return result_string
+
 
 
